c7515f3e35e309a82722f49f6099edb6bdad8c2d
Rudransh Bhardwaj <aarti19830@gmail.com>
2024-01-15 17:51:31 +0530

    done @rsh

--- main.py
+++ main.py
@@ -28,41 +28,23 @@ def is_solved(Uface, Dface, Lface, Rface, Fface, Bface):
 
     return True
 
-def turnL2(Uface, Dface , Lface ,Rface , Fface , Bface):
-    Uface[0][0] , Dface[1][0] = swap(Uface[0][0],Dface[1][0])
-    Uface[1][0] , Dface[0][0] = swap(Uface[1][0],Dface[0][0])
+def turnU2(Uface, Dface, Lface, Rface, Fface, Bface):
+    Uface[0][0], Dface[0][1] = swap(Uface[0][0], Dface[0][1])
+    Uface[0][1], Dface[0][0] = swap(Uface[0][1], Dface[0][0])
 
-    Lface[0], Lface[1] = swap(Lface[0],Lface[1]) 
-    Fface[0], Bface[0] = swap(Fface[0],Bface[0])
+    Lface[0], Rface[0] = swap(Rface[0], Lface[0])
 
-    return Uface, Dface, Lface, Rface, Fface, Bface
-
-def turnR2(Uface, Dface , Lface ,Rface , Fface , Bface):
-    Uface[0][1] , Dface[1][1] = swap(Uface[0][1],Dface[1][1])
-    Uface[1][1] , Dface[0][1] = swap(Uface[1][1],Dface[0][1])
-
-    Rface[0], Rface[1] = swap(Rface[0],Rface[1]) 
-    Fface[1], Bface[1] = swap(Fface[1],Bface[1])
+    Fface[0], Fface[1] = swap(Fface[0], Fface[1])
+    Bface[0], Bface[1] = swap(Bface[0], Bface[1])
 
     return Uface, Dface, Lface, Rface, Fface, Bface
 
-def turnF2(Uface, Dface, Lface, Rface, Fface, Bface):
-    Uface[0][0], Rface[0] = swap(Rface[0], Uface[0][0])
-    Uface[0][1], Rface[1] = swap(Rface[1], Uface[0][1])
-    Dface[0][0], Lface[0] = swap(Lface[0], Dface[0][0])
-    Dface[0][1], Lface[1] = swap(Lface[1], Dface[0][1])
+def turnR2(Uface, Dface, Lface, Rface, Fface, Bface):
+    Uface[0][1], Dface[1][1] = swap(Uface[0][1], Dface[1][1])
+    Uface[1][1], Dface[0][1] = swap(Uface[1][1], Dface[0][1])
 
-    Fface[0], Fface[1] = swap(Fface[1], Fface[0])
-
-    return Uface, Dface, Lface, Rface, Fface, Bface
-
-def turnB2(Uface, Dface, Lface, Rface, Fface, Bface):
-    Uface[0][1], Rface[0] = swap(Rface[0], Uface[0][1])
-    Uface[1][1], Rface[1] = swap(Rface[1], Uface[1][1])
-    Dface[0][1], Lface[0] = swap(Lface[0], Dface[0][1])
-    Dface[1][1], Lface[1] = swap(Lface[1], Dface[1][1])
-
-    Bface[0], Bface[1] = swap(Bface[1], Bface[0])
+    Rface[0], Rface[1] = swap(Rface[1], Rface[0])
+    Fface[1], Bface[1] = swap(Fface[1], Bface[1])
 
     return Uface, Dface, Lface, Rface, Fface, Bface
 
@@ -73,16 +55,12 @@ def generate_moves(cube_state, depth, move_sequence):
             return True
         return False
 
-    for move in ['L2', 'R2', 'F2', 'B2']:
-        new_state = cube_state
-        if move == 'L2':
-            new_state = turnL2(*new_state)
+    for move in ['U2', 'R2']:
+        new_state = [list(row) if isinstance(row, list) else row for row in cube_state]
+        if move == 'U2':
+            new_state = turnU2(*new_state)
         elif move == 'R2':
             new_state = turnR2(*new_state)
-        elif move == 'F2':
-            new_state = turnF2(*new_state)
-        elif move == 'B2':
-            new_state = turnB2(*new_state)
 
         if generate_moves(new_state, depth - 1, move_sequence + [move]):
             return True
@@ -90,19 +68,23 @@ def generate_moves(cube_state, depth, move_sequence):
     return False
 
 def solve_2x2x1(cube_state):
+    if is_solved(*cube_state):
+        print("It is already solved!")
+        return True
     depth = 1
     while not generate_moves(cube_state, depth, []):
         depth += 1
 
 # Initial state
-U = [[1, 1],
-     [1, 1]]
-D = [[2, 2],
-     [2, 2]]
-L = [3, 3]
-R = [4, 4]
-F = [5, 5]
-B = [6, 6]
+U = [[2, 1],
+     [2, 1]]
+D = [[1, 2],
+     [1, 2]]
+L = [4, 4]
+R = [3, 3]
+F = [5, 6]
+B = [6, 5]
+
 
 # Solve the cube
-solve_2x2x1((U, D, L, R, F, B))
+solve_2x2x1([U, D, L, R, F, B])
